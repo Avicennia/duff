7149defbe945d5934325f2028ed6d116dc254868
Tea <a.germinans@gmail.com>
2021-04-06 04:49:48 -0700

    testure changes, abm logic tweaks

--- init.lua
+++ init.lua
@@ -1,9 +1,10 @@
+-- luacheck: globals duff minetest _ ItemStack nodecore
+
 -- LUALOCALS -- -- -- -- -- -- -- --
 local minetest, nodecore
     = minetest, nodecore
 -- LUALOCALS -- -- -- -- -- -- -- --
 local modn = minetest.get_current_modname()
-local function say(x) return minetest.chat_send_all(type(x) == "string" and x or minetest.serialize(x)) end
 
 duff = {}
 local duff = duff
@@ -17,7 +18,7 @@ end
 
 duff.node_duff_map = {} -- key-value array storage for accessing all duffs that a node may have, [nodename] = {duffname1, duffname2}
 duff.collate_source_nodes = function() -- Searches all registered duffs to register duffs to specific node sources
-    for k,v in pairs(duff.duffs)do
+    for _,v in pairs(duff.duffs)do
         if(v.source_node and v.particle_def)then
             local nodename = v.source_node
             local nodegroups = minetest.registered_nodes[nodename].groups
@@ -39,7 +40,7 @@ end
 --  --  --  --  --  WIND
 duff.wind = {x = 0, y = 0, z = 0} -- Controls direction that generated particles move in
 duff.blow_wind_random = function() -- Makes wind randomly change direction within some arbitrary params
-    duff.wind = {x = math.random(100)/100, y = -math.random(40)/100 ,z = math.random(100)/100}
+    duff.wind = {x = math.random(10)/100, y = -math.random(40)/100 ,z = math.random(100)/100}
 end
 duff.blow_wind_vector = function(v) -- Makes wind blow in a desired vector direction
     local function is_num(n)
@@ -51,42 +52,46 @@ duff.blow_wind_vector = function(v) -- Makes wind blow in a desired vector direc
     end
 end
 
-nodecore.interval(60, function() duff.blow_wind_random()end)
+nodecore.interval(60, function() duff.blow_wind_random()end) -- reroll wind direction every minute
 
 --  --  --  --  --  PARTICLES
 
-duff.shed_particle = function(pos, duff_def) -- Causes particles to fall as defined by duff definition
-    if(pos and duff_def)then
+duff.shed_particle = function(pos, duff_particle_def) -- Causes particles to fall as defined by duff definition
+    if(pos and duff_particle_def)then
+        local particle_def = duff_particle_def
         local bas = {x = 0, y = 0, z = 0}
-        duff_def.minpos = {x = pos.x - 0.5, y = pos.y-0.4, z = pos.z - 0.5}
-        duff_def.maxpos = {x = pos.x + 0.5, y = pos.y-0.3, z = pos.z + 0.5}
-        duff_def.minvel = duff_def.minvel or bas
-        duff_def.maxvel = duff_def.maxvel or duff.wind or {x = 0.01, y = -0.5, z = 0.01}
-        minetest.add_particlespawner(duff_def)
+        
+        particle_def.minpos = {x = pos.x - 0.5, y = pos.y-0.5, z = pos.z - 0.5}
+        particle_def.maxpos = {x = pos.x + 0.5, y = pos.y-0.5, z = pos.z + 0.5}
+        particle_def.minvel = particle_def.minvel or bas
+        particle_def.maxvel = particle_def.maxvel or duff.wind or {x = 0.01, y = -0.5, z = 0.01}
+        
+        minetest.add_particlespawner(particle_def)
     end
 end
 
 
-
+-- Load in register
 local modp = minetest.get_modpath(modn)
 dofile(modp.."/register.lua")
 duff.collate_source_nodes()
 
 
 
-
+--  --  --  --  --  SHEDDING
 nodecore.register_limited_abm(
     {
-        label = "Dirty Little Littering Node",
+        label = "Node shedding duff",
         nodenames = {"group:duffy"},
         neighbors = {"air"},
-        interval = 1,
-        chance = 10,
+        interval = 6,
+        chance = 40,
         catch_up = false,
-        action = function(pos,node)
-            local nodename = node.name
+        action = function(pos)
             local pos_under = {x = pos.x, y = pos.y - 1, z = pos.z}
+            
             if(minetest.get_node(pos_under).name == "air")then
+                
                 for _,v in pairs(duff.duffs)do
                     local pdef = v.particle_def
                     duff.shed_particle(pos, pdef)
@@ -104,35 +109,44 @@ nodecore.register_limited_abm(
         label = "Duff Settle Global",
         nodenames = {"group:duffy"},
         neighbors = {"air"},
-        interval = 4,--120
-        chance = 10,--10
+        interval = 120,
+        chance = 10,
         catch_up = true,
         action = function(pos,node)
             local nodename = node.name
             local pos_under = {x = pos.x, y = pos.y - 1, z = pos.z}
-            local max_depth = {x = pos.x, y = pos.y - 128, z = pos.z}
-            local is_floor,where_floor = minetest.line_of_sight(pos_under, max_depth)
-            local floor_has_duff = where_floor and minetest.get_item_group(minetest.get_node(where_floor).name, "duff") > 0
-            where_floor.y = where_floor.y + 1
-            where_floor = where_floor and minetest.get_node(where_floor).name == "air" and where_floor
-            if((not is_floor) and where_floor and nodename and minetest.get_node(pos_under).name == "air" and (not floor_has_duff))then
-                local node_duffs = duff.node_duff_map[nodename]
-                if(node_duffs and #node_duffs > 0)then
-                    for n = 1, #node_duffs do
-                        local duff_def = duff.duffs[node_duffs[n]]
-                        local def = duff_def.settle_def
-                        local chance = math.random(1000)
-                        if(chance > def.chance)then
-                            minetest.set_node(where_floor,{name = def.placenode})
+            local space_under_node = minetest.get_node(pos_under).name == "air"
+
+            if(space_under_node)then
+            
+                local max_depth = {x = pos.x, y = pos.y - 128, z = pos.z}
+                local is_floor,where_floor = minetest.line_of_sight(pos_under, max_depth)
+
+                if(where_floor)then -- checking for suitable position underneath
+                    local floor_node = minetest.get_node(where_floor).name
+                    where_floor = minetest.registered_nodes[floor_node].drawtype == "normal" and where_floor -- make sure odd drawtypes are not selected for placement.
+                end
+
+            
+                if((not is_floor) and where_floor and nodename and (not floor_has_duff))then
+                    local node_duffs = duff.node_duff_map[nodename]
+
+                    if(node_duffs and #node_duffs > 0)then
+
+                        for n = 1, #node_duffs do
+
+                            local duff_def = duff.duffs[node_duffs[n]]
+                            local def = duff_def.settle_def
+                            local chance = math.random(1000) -- to give later definitions some control of spawn chance within this single abm reg
+                            local placenode = def.placenode
+
+                            if(chance > def.chance and placenode and minetest.registered_nodes[placenode.name])then
+                                where_floor.y = where_floor.y + 1
+                                minetest.set_node(where_floor,placenode) -- place designated duff
+                            end
                         end
                     end
                 end
             end
-        end
+        end -- end pyramids ftw, embrace the Gerold
     })
-
-    local function test(x,dothing)
-        x = x^2
-        dothing(x)
-    end
-    minetest.after(5, function() test(5, function(k) say(k) end)end)
--- register.lua
+++ register.lua
@@ -1,43 +1,53 @@
+-- luacheck: globals duff minetest _ ItemStack nodecore
+
 local modn = minetest.get_current_modname()
-local prepare_for_winter = minetest.registered_nodes["nc_nature:dirt_with_leaf_litter"]
 --  --  --  -- Leaf Litter  --  --  --  --
+
+--  --  --  --  --  DUFF DEFINITION
+for n = 1, 2 do
+    local a = n == 1
 duff.register_duff({
     source_node = "nc_tree:leaves",
-    name = "leaf_litter",
+    
+    name = "leaf_litter_"..n,
+    
     settle_def = {
         chance = 989,
         neighbors = {},
-        catch_up = false,
-        placenode = modn..":leaflitter1"
+        catch_up = true,
+        placenode = {name = modn..":leaflitter1"}
     },
+    
     particle_def = {
-        amount = 2,
+        amount = 4,
         time = 1,
         minacc = {x = -0.1, y = -0.5, z = -0.1},
         maxacc = {x = 0.1, y = -0.5, z = 0.1},
         minexptime = 4,
         maxexptime = 6,
-        minsize = 1.2,
-        maxsize = 2.2,
+        minsize = a and 1.2 or 0.5,
+        maxsize = a and 2 or 1,
 
         collisiondetection = true,
-        collision_removal = true,
-        vertical = true,
-        texture = "duff_spore2.png",
+        --collision_removal = true,
+        vertical = false,
+        texture = "duff_leaf"..n..".png",
         animation = {
             type = "vertical_frames",
             aspect_w = 16,
             aspect_h = 16,
-            length = 2},
+            length = 1},
         {
             type = "sheet_2d",
             frames_w = 1,
-            frames_h = 9,
-            frame_length = 2/9,
+            frames_h = a and 9 or 18,
+            frame_length = a and 1/9 or 1/18,
         },
-        glow = 12
+        glow = 2,
     }
 })
+end
+--  --  --  --  --  NODE REGISTRATIONS
 for n = 1, 3 do
 minetest.register_node(modn..":leaflitter"..n, {
     description = "leaflitter"..n..".png",
@@ -58,39 +68,59 @@ minetest.register_node(modn..":leaflitter"..n, {
 	},
     groups = {green = 2, flammable = 2, falling_repose = 2, falling = 1, duff = 1, duff_leaflitter = 1, loose = 1},
     on_ignite = "nc_fire:ash_lump",
-    on_punch = function(pos,node)
-    end
 })
 end
+--  --  --  --  --  LEAF LITTER GRINDING RECIPE
+nodecore.register_craft({
+    label = "grind leaf duff to peat",
+    action = "pummel",
+    indexkeys = {"group:duff"},
+    toolgroups = {crumbly = 2},
+    nodes = {
+        {
+            match = {groups = {duff = true}, count = 24},
+            replace = "nc_tree:peat"
+        }
+    }
+})
+
+
+--  --  --  --  --  ABM AND PERSISTENT BEHAVIOUR
 nodecore.register_limited_abm(
     {
         label = "Fallen leaf litter logic",
         nodenames = {"group:duff_leaflitter"},
-        interval = 90, -- 60 or 126
+        interval = 240,
         chance = 10,
         catch_up = true,
         action = function(pos,node)
             local nodename = node.name
             local pos_under = {x = pos.x, y = pos.y - 1, z = pos.z}
-            local chance = math.random(1000)
+            local chance = math.random(10)
             local new_param2 = math.random(0,3)
-            if(chance > 70)then
+            
+            if(chance > 7)then
                 local function leaflitter_decay()
                     local digit = string.find(nodename,"%d") -- find the number in the nodename
                     local lv = digit and tonumber(string.sub(nodename,digit))
+
                     if(lv < 3)then
+                       
                         minetest.set_node(pos,{name = modn..":leaflitter"..lv+1, param2 = new_param2}) -- place next decay stage if not at max decay
-                    elseif(lv == 3 and minetest.get_item_group(minetest.get_node(pos_under).name,"soil") > 0)then
-                        local half_chance = math.random(2) == 2
+                    
+                    elseif(lv == 3 and nodecore.node_group("soil",pos_under))then
+                        
+                        local half_chance = chance > 5
                         if(half_chance)then
-                            local node = prepare_for_winter and "nc_nature:dirt_with_leaf_litter" or "nc_tree:peat"
-                            minetest.set_node(pos_under,{name = node}) -- else let decay stage become incorporated into underlying soil as peat
+                            local prepare_for_winter = minetest.registered_nodes["nc_nature:dirt_with_leaf_litter"] -- check if nc_nature's node is registered
+                            local node_repl = prepare_for_winter and "nc_nature:dirt_with_leaf_litter" or "nc_tree:humus"
+                            minetest.set_node(pos_under,{name = node_repl}) -- else let decay stage become incorporated into underlying soil as peat
                         end
-                            minetest.remove_node(pos)
+                        minetest.remove_node(pos)
                     end
                 end
                 leaflitter_decay()
-            elseif(chance > 876 and nodename ~= modn..":leaflitter3")then
+            elseif(chance > 5 and nodename ~= modn..":leaflitter3")then
                 local function leaflitter_relocate()
                     local p1 = {x = pos.x + 1, y = pos.y, z = pos.z + 1} -- corner1
                     local p2 = {x = pos.x - 1, y = pos.y, z = pos.z - 1} -- corner2
@@ -100,8 +130,8 @@ nodecore.register_limited_abm(
                         local chosen_air = near_air_nodes[math.random(#near_air_nodes)] -- randomly choose a node from the options
                         local near_air_below = {x = chosen_air.x, y = chosen_air.y - 1, z = chosen_air.z}
 
-                        suitable_node_below = minetest.get_node(near_air_below).name ~= "air" and
-                        minetest.get_item_group(minetest.get_node(near_air_below).name, "duff_leaflitter") == 0 -- node must not have air or another leaflitter node underneath it
+                        local suitable_node_below = minetest.get_node(near_air_below).name ~= "air" and
+                        (not nodecore.node_group("duff",near_air_below)) -- node must not have air or another leaflitter node underneath it
 
                         if(chosen_air and suitable_node_below)then -- perform placement and removal for relocation
                             minetest.set_node(chosen_air, {name = nodename, param2 = new_param2}) -- replace node
